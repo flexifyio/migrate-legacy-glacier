ac8e533afed93e19f426deec60ef19036daf50f1
Sergey Kandaurov <sk@flexify.io>
2021-05-15 19:30:51 +0800

    Recmoved keys from config

--- src/config/config.py
+++ src/config/config.py
@@ -7,20 +7,20 @@ INVENTORY_JSON = ''
 # inventory split file name in /src/output_step1 e.g. 'input_split_1'
 PROCESSING_SPLIT = ''
 
-UPLOAD_THREADS = 5
+UPLOAD_THREADS = 8
 
 # size of a chunk downloaded from vault (6 mb)
-CHUNK_SIZE = 6 * 1024 * 1024
+CHUNK_SIZE = 32 * 1024 * 1024
 
 # glacier
-GL_KEY = 'AKIAVHCLHRT4DJF652NK'
-GL_SECRET = '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'
-GL_VAULT = 'flexify-glacier'
+GL_KEY = ''
+GL_SECRET = ''
+GL_VAULT = ''
 GL_REGION = 'us-east-1'
 GL_TIER = 'Bulk'
 
 # s3
-S3_KEY = 'AKIAIVW6TZW6Q4MBZZ7A'
-S3_SECRET = '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'
-S3_BUCKET = 'flexify'
+S3_KEY = ''
+S3_SECRET = ''
+S3_BUCKET = ''
 S3_REGION = 'us-east-1'
