71cea8ac3f6e025d9b5fbb95f9868b040cab0bcf
Sergey Kandaurov <sk@flexify.io>
2021-05-16 15:54:03 +0800

    Attempt to fix memory leack

--- src/step3.py
+++ src/step3.py
@@ -12,20 +12,25 @@ def migrate():
         list_jobs = input_file.readlines()
 
     with open(f'output_step3/completed_jobs', "w", encoding='utf-8') as output_file:
-        with ThreadPoolExecutor(max_workers=UPLOAD_THREADS) as executor:
-            futures = []
-            for job in list_jobs:
-                job_id, arch_path, arch_size = job.split('|||')
-                migration_service = MigrationService()
-                futures.append(executor.submit(migration_service.migrate, job_id=job_id, key=arch_path, size=int(arch_size)))
+        with open(f'output_step3/errors', "w", encoding='utf-8') as errors_file:
+            with ThreadPoolExecutor(max_workers=UPLOAD_THREADS) as executor:
+                futures = []
+                for job in list_jobs:
+                    job_id, arch_path, arch_size = job.split('|||')
+                    migration_service = MigrationService()
+                    futures.append(executor.submit(migration_service.migrate, job_id=job_id, key=arch_path, size=int(arch_size)))
 
-            for future in concurrent.futures.as_completed(futures):
-                try:
-                    output_file.write(future.result() + '\n')
-                except Exception as e:
-                    logging.error(e)
+                while futures:
+                    future = futures[0]
+                    try:
+                        output_file.write(future.result() + '\n')
+                    except Exception as e:
+                        logging.error(e)
+                        errors_file.write('Exception: %s\n' % (e))
+                    futures.pop(0)
 
 
 if __name__ == '__main__':
     initialize_logging()
     migrate()
+    logging.info('Done.')
