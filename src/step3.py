9e80106e48fe82679c1e71ac1720642e6d76d60f
Sergey Kandaurov <sk@flexify.io>
2021-05-18 19:07:29 +0800

    Multi-streaming in step3

--- src/step3.py
+++ src/step3.py
@@ -15,22 +15,37 @@ def migrate():
 
     with open(f'output_step3/completed_jobs', "w", encoding='utf-8') as output_file:
         with open(f'output_step3/errors', "w", encoding='utf-8') as errors_file:
-            for job in list_jobs:
-                job_id, arch_path, arch_size = job.split('|||')
-                migration_service = MigrationService()
-                try:
-                    result = migration_service.migrate(job_id=job_id, key=arch_path, size=int(arch_size))
-                    logging.info('DONE job %s' % result)
-                    output_file.write(result + '\n')
-                    output_file.flush()
-                except Exception as e:
-                    logging.error(e)
-                    errors_file.write('EXCEPTION: %s\n' % (e))
-                    errors_file.flush()
-                del migration_service
-                logging.debug('Collected %s', gc.collect())
+            with ThreadPoolExecutor(max_workers=UPLOAD_THREADS) as executor:
+                futures = []
+                while list_jobs or futures:
+                    # add theads up to target value
+                    while list_jobs and len(futures) < UPLOAD_THREADS:
+                        job = list_jobs.pop(0)
+                        job_id, arch_path, arch_size = job.split('|||')
+                        migration_service = MigrationService()
+                        futures.append(executor.submit(migration_service.migrate, job_id=job_id, key=arch_path, size=int(arch_size)))
+
+                    # wait of at least on thread to complete
+                    finished, unfinished = concurrent.futures.wait(futures, timeout=None, return_when=FIRST_COMPLETED)
+                    logging.debug('FINISHED %s future(s)', len(finished))
+                    for future in finished:
+                        try:
+                            result = future.result()
+                            logging.info('DONE job %s' % result)
+                            output_file.write(result + '\n')
+                            output_file.flush()
+                            logging.info('SAVED job %s' % result)
+                        except Exception as e:
+                            logging.error(e)
+                            errors_file.write('EXCEPTION: %s\n' % (e))
+                            errors_file.flush()
+                        futures.remove(future)
+
+                    # clean up memory
+                    del finished
+                    logging.debug('Collected %s', gc.collect())
+                logging.info('ALL DONE.')
 
 if __name__ == '__main__':
     initialize_logging()
     migrate()
-    logging.info('Done.')
